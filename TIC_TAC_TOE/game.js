62ae8f21456cbb2dd887305b8064c0ab8f3eead4
Tonny Blair <jusanotherthursday@gmail.com>
2025-12-27 23:42:35 +0300

    Update game.js
    The game logic is now fully complete

--- TIC_TAC_TOE/game.js
+++ TIC_TAC_TOE/game.js
@@ -14,23 +14,71 @@ let gameArray = Array(9).fill("");
 //current Player
 let currentPlayer = 'x';
 
+//game status
+let gameActive = true;
+
+// Win Conditions
+let winPattern = [
+	[0,1,2],[3,4,5],[6,7,8],
+	[0,3,6],[1,4,7],[2,5,8],
+	[0,4,8],[2,4,6]
+];
+
+// Result Validation 
+function handleResultValidation(){
+	for(let i = 0; i < winPattern.length; i++){
+		console.log(winPattern[i]);
+		
+		let a = winPattern[i][0];
+		let b = winPattern[i][1];
+		let c = winPattern[i][2];
+
+		if(gameArray[a] !== "" && gameArray[a] === gameArray[b] && gameArray[b] === gameArray[c]){
+			console.log("dude, you made !");
+			gameActive = false;
+
+			setTimeout( () => {
+				alert(`Game Over. ${gameArray[a]} won !`);
+			}, 1000);
+
+			return; // stop the loop after a win is detected.
+		}
+	}
+
+	// Check for a draw(after win check.)
+	if(!gameArray.includes("")){
+		gameActive = false; //stop the board.
+
+		setTimeout( () => {
+			alert(`Game Over. It's a draw!`);
+		}, 1000);
+		return;
+	}	
+}
+
 //Event Delegation
 const gameTable = document.querySelector('.game-table');
 
 gameTable.addEventListener('click', (e) => {
 	//console.log(e);
+	if(!gameActive) return;
+
 	if(e.target.classList.contains('game-cell')){
 
 		let cellNumber = parseInt(e.target.dataset.index);
 
 		console.log('Cell clicked:', cellNumber);
 		
-		if(gameArray[cellNumber] == 'x' || gameArray[cellNumber] == 'o'){
+		if(gameArray[cellNumber] !== ""){
 			console.log("aye aye Captain");
 		}else{
 			gameArray[cellNumber] = currentPlayer;
 			e.target.textContent = currentPlayer;
-			currentPlayer = currentPlayer === 'x' ? 'o' : 'x';
+			handleResultValidation();
+			
+			if(gameActive){
+				currentPlayer = currentPlayer === 'x' ? 'o' : 'x';
+			}
 		}
 		
 	}
@@ -38,4 +86,3 @@ gameTable.addEventListener('click', (e) => {
 });
 
 console.log(gameArray);
-
