aeb80d587a5171899b83d1c3198076160f77e386
opblair <jusanotherthursday@gmail.com>
2026-01-05 23:25:07 +0300

    Added a function to generate the board and pieces

--- checkers/checkers.js
+++ checkers/checkers.js
@@ -1,1 +1,37 @@
+"use strict";
+const gameBoard = document.getElementById('game-board');
 
+let pieces = [];
+
+function createBoard(){
+    for (let row = 0; row < 8; row++) {
+        for (let col = 0; col < 8; col++){
+            let  square = document.createElement('div');
+            square.classList.add('square');
+
+            if( (row + col) % 2 === 0){
+                square.classList.add('light');
+
+            }else{
+                square.classList.add('dark');
+
+                if(row < 3){
+                    let piece = document.createElement('div');
+                    piece.classList.add('piece');
+                    piece.style.backgroundColor = "red";
+                    square.appendChild(piece);
+                    pieces.push({row, col, player: 1});
+                }else if(row > 4){
+                    let piece = document.createElement('div');
+                    piece.classList.add('piece');
+                    piece.style.backgroundColor = "black";
+                    square.appendChild(piece);
+                    pieces.push({row, col, player: 2});
+                }
+            }
+            gameBoard.appendChild(square);
+        }
+    }
+}
+
+createBoard();
