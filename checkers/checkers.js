1b07074f8b6623a6e1e786edd578196fac383808
opblair <jusanotherthursday@gmail.com>
2026-01-06 21:50:43 +0300

    added cell index

--- checkers/checkers.js
+++ checkers/checkers.js
@@ -9,22 +9,23 @@ function createBoard(){
             let  square = document.createElement('div');
             square.classList.add('square');
 
+            let cellIndex = row * 8  + col;
+            square.dataset.cell = cellIndex;
             if( (row + col) % 2 === 0){
                 square.classList.add('light');
-
             }else{
                 square.classList.add('dark');
-
+                
                 if(row < 3){
                     let piece = document.createElement('div');
                     piece.classList.add('piece');
-                    piece.style.backgroundColor = "red";
+                    piece.style.backgroundColor = "black";
                     square.appendChild(piece);
                     pieces.push({row, col, player: 1});
                 }else if(row > 4){
                     let piece = document.createElement('div');
                     piece.classList.add('piece');
-                    piece.style.backgroundColor = "black";
+                    piece.style.backgroundColor = "rgb(249, 248, 248)";
                     square.appendChild(piece);
                     pieces.push({row, col, player: 2});
                 }
@@ -35,3 +36,8 @@ function createBoard(){
 }
 
 createBoard();
+console.log(pieces);
+
+//Index=(Row×TotalColumns)+Column
+//Row=floor(Index/8)
+//Col=Index(mod8)
