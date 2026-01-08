d294fcdc2d0513fd31bbf6072a4b213c04cc94cb
opblair <jusanotherthursday@gmail.com>
2026-01-08 22:26:09 +0300

    Added a variable to track the current selected piece, it stores values using an object, in a key value pair format

--- checkers/checkers.js
+++ checkers/checkers.js
@@ -3,6 +3,7 @@ const gameBoard = document.getElementById('game-board');
 
 //let boardState = Array.from({length: 8}, () => Array(8).fill(null));
 let boardState = [];
+let selectedPiece = null;
 function createBoard(){
     for (let row = 0; row < 8; row++) {
         let rowArray = []
@@ -42,14 +43,20 @@ gameBoard.addEventListener('click', (e) =>{
     if(e.target.classList.contains("piece")){
         console.log("Clicked piece on cell:", e.target.dataset.cell);
         console.log("Player:",e.target.dataset.player);
+        const fromRow = Math.floor((parseInt(e.target.dataset.cell)) / 8);
+        const fromCol = (parseInt(e.target.dataset.cell)) % 8;
+        selectedPiece = {
+            fromRow: fromRow,
+            fromCol: fromCol
+            //player: player
+        };
     }else if(e.target.classList.contains("square")){
         console.log("Clicked cell:", e.target.dataset.cell);
     }
-    console.log(`Clicked Cell: ${e.target.dataset.cell}`);
 })
 
-console.log(gameBoard);
 console.log(boardState);
+
 
 //Index=(Row×TotalColumns)+Column
 //Row=floor(Index/8)
