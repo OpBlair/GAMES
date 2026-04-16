3d23d76778cdc494d803d70ac71c0960918132cc
opblair <jusanotherthursday@gmail.com>
2026-04-16 21:04:03 +0300

    added communication

--- checkers/Checkers.Backend/Hubs/GameHub.cs
+++ checkers/Checkers.Backend/Hubs/GameHub.cs
@@ -1,16 +1,39 @@
 using Microsoft.AspNetCore.SignalR;
 using Checkers.Backend.Game;
 
-public class GameHub : Hub
+namespace Checkers.Backend.Hubs
 {
-    private static CheckersEngine game = new CheckersEngine();
-
-    public async Task SendMove(int fromRow, int fromCol, int toRow, int toCol)
+    public class GameHub : Hub
     {
-        var valid = game.ApplyMove(fromRow, fromCol, toRow, toCol);
+        private readonly CheckersEngine _engine;
+        public GameHub(CheckersEngine engine) => _engine = engine;
 
-        if(!valid) return;
+        public async Task StartGame()
+        {
+            _engine.CreateInitialBoard();
+            await Clients.All.SendAsync("GameStarted", _engine.Board, _engine.CurrentPlayer);
+        }
 
-        await Clients.All.SendAsync("RecieveBoard", game.Board);
+        public async Task MakeMove(int fR, int fC, int tR, int tC)
+        {
+            // Find the move in legal moves to get the AttackRow/Col data
+            var legalMoves = CheckersRules.GetLegalMoves(_engine, fR, fC);
+            var move = legalMoves.FirstOrDefault(m => m.ToRow == tR && m.ToCol == tC);
+
+            if (move != null && _engine.ExecuteMove(fR, fC, move))
+            {
+                await Clients.All.SendAsync("MoveMade", _engine.Board, _engine.CurrentPlayer, _engine.MustJumpPiece);
+            }
+            else
+            {
+                await Clients.Caller.SendAsync("InvalidMove");
+            }
+        }
+
+        public async Task ForfeitGame() => 
+            await Clients.All.SendAsync("GameEnded", $"Player {_engine.CurrentPlayer} forfeited.");
+
+        public override async Task OnDisconnectedAsync(Exception? ex) => 
+            await base.OnDisconnectedAsync(ex);
     }
 }
