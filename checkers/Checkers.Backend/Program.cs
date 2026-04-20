94280840f560dcf9f6371a926ef2443d99eb9336
opblair <jusanotherthursday@gmail.com>
2026-04-20 03:22:08 +0300

    switched from global CheckersEngine singleton approach to GameManager Dependency Injection(DI)

--- checkers/Checkers.Backend/Program.cs
+++ checkers/Checkers.Backend/Program.cs
@@ -4,7 +4,7 @@ using Checkers.Backend.Hubs; // Points to GameHub
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSignalR();
-builder.Services.AddSingleton<CheckersEngine>();
+builder.Services.AddSingleton<GameManager>();
 
 builder.Services.AddCors(options => {
     options.AddPolicy("AllowFrontend", policy => {
