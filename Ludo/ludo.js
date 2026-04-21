14285bc5372f3a1bbae9d90c527280cafda45b2a
opblair <jusanotherthursday@gmail.com>
2026-04-21 20:50:03 +0300

    Added turn switching for the players.

--- Ludo/ludo.js
+++ Ludo/ludo.js
@@ -4,33 +4,38 @@ const gameBoard = document.getElementById('game-board');
 const dice = document.getElementById('dice');
 const vsHuman = document.getElementById('human');
 const welcomeScreen = document.querySelector('.welcome');
+
 // ------ BUTTON FOR PLAYING AGAINST HUMAN --------
 vsHuman.addEventListener('click', () => {
   welcomeScreen.style.display = 'none';  
   gameBoard.style.display = 'grid';
   dice.style.display = 'grid';
 })
+
 // --------- HOME BASE ----------
 let redBase = [0,1,2,3,4,5,15,16,17,18,19,20,30,31,32,33,34,35,45,46,47,48,49,50,60,61,62,63,64,65,75,76,77,78,79,80];
 let greenBase = [9,10,11,12,13,14,24,25,26,27,28,29,39,40,41,42,43,44,54,55,56,57,58,59,69,70,71,72,73,74,84,85,86,87,88,89];
 let yellowBase = [144,145,146,147,148,149,159,160,161,162,163,164,174,175,176,177,178,179,189,190,191,192,193,194,204,205,206,207,208,209,219,220,221,222,223,224];
 let blueBase = [135,136,137,138,139,140,150,151,152,153,154,155,165,166,167,168,169,170,180,181,182,183,184,185,195,196,197,198,199,200,210,211,212,213,214,215];
+
 // --------- HOME PATHS ----------
 let redPath = [91,106,107,108,109,110,111];
 let greenPath = [22,23,37,52,67,82,97];
 let yellowPath = [113,114,115,116,117,118,133];
 let bluePath = [127,142,157,172,187,201,202];
+
 // ------- FULL TOKEN PATHS ------
 let gamePath = Array(51).fill(null);
-console.log(gamePath);
+
 // -------- START POSITIONS ------
 let redStart = 2;
 let greenStart = 18;
 let yellowStart = 30;
 let blueStart = 48;
+
 // -------- CREATE BOARD ----------
 function createBoard(){
-    console.log("am running");
+    
     // ludo board = 15x15(225 squares)
     for(let i = 0; i < 225; i++){
         let square = document.createElement('div');
@@ -65,6 +70,7 @@ function createBoard(){
         }
     }
 }
+
 // Function to add tokens(pieces)
 function addTokens(){
 
@@ -86,6 +92,7 @@ function addTokens(){
         })
     }
 }
+
 // Dice Pattern
 const dicePatterns = {
     1: [4],
@@ -95,9 +102,13 @@ const dicePatterns = {
     5: [0, 2, 4, 6, 8],
     6: [0, 2, 3, 5, 6, 8]
 };
+
 // Function to display number on the dice
 function rollDice(){
+    if(!gameState.canRoll) return;
+
     // animation
+    gameState.canRoll = false;
     dice.classList.add('rolling');
     dice.innerHTML = '';
 
@@ -105,11 +116,17 @@ function rollDice(){
         dice.classList.remove('rolling');
 
         let number = Math.floor((Math.random() * 6) + 1);
-        //dice.textContent = number;
         gameState.diceValue = number;
-        //return number;
         renderDiceDots(number);
-        console.log("Rolled:", number);
+
+        setTimeout(() => {
+            if(number !== 6){
+                switchTurn();
+            }else{
+                console.log("Rollled a 6! Roll again.");
+                gameState.canRoll = true;
+            }
+        }, 1000);
     }, 500);
 }
 
@@ -127,6 +144,7 @@ function renderDiceDots(number){
         dice.appendChild(dotSlot);
     }
 }
+
 // ---- DICE ROLL EVENT LISTENER -----
 dice.addEventListener('click', () => {
     if(!dice.classList.contains('rolling')){
@@ -134,21 +152,35 @@ dice.addEventListener('click', () => {
     }
 });
 renderDiceDots(1);
+
 // ---- MOVE TOKEN FUNCTION ----
 function moveToken(token, numberOfSteps){
 
 }
+
 // ---- GAME STATE -----
 const gameState = {
-    currentPlayer: '',
+    currentPlayerIndex: 0,
+    players: ['red', 'green', 'yellow', 'blue'],
     diceValue: null,
     isGameOver: false,
+    canRoll: true,
     positions: {
         red: [16, 19, 61, 64],       
         green: [25, 28, 70,73],
         yellow: [160, 163, 205, 208],
         blue: [151, 154, 196, 199]
     }
+}
+
+// ----- FUNCTION TO SWITCH PLAYER TURNS -----
+function switchTurn(){
+    gameState.currentPlayerIndex = (gameState.currentPlayerIndex + 1) % 4;
+    gameState.canRoll = true;
+
+    const nextPlayer = gameState.players[gameState.currentPlayerIndex];
+    dice.style.borderColor = nextPlayer;
+    console.log(`It is now ${nextPlayer}'s turn`);
 }
 createBoard();
 addTokens();
