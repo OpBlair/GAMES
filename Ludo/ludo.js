845303948e6c597f832d4b3edb87fa2bafeb4dbc
opblair <jusanotherthursday@gmail.com>
2026-04-25 21:11:58 +0300

    Rearranged functions to fix flow of logic.

--- Ludo/ludo.js
+++ Ludo/ludo.js
@@ -5,18 +5,11 @@ const dice = document.getElementById('dice');
 const vsHuman = document.getElementById('human');
 const welcomeScreen = document.querySelector('.welcome');
 
-// ------ BUTTON FOR PLAYING AGAINST HUMAN --------
-vsHuman.addEventListener('click', () => {
-  welcomeScreen.style.display = 'none';  
-  gameBoard.style.display = 'grid';
-  dice.style.display = 'grid';
-})
-
 // --------- HOME BASE ----------
-let redBase = [0,1,2,3,4,5,15,16,17,18,19,20,30,31,32,33,34,35,45,46,47,48,49,50,60,61,62,63,64,65,75,76,77,78,79,80];
-let greenBase = [9,10,11,12,13,14,24,25,26,27,28,29,39,40,41,42,43,44,54,55,56,57,58,59,69,70,71,72,73,74,84,85,86,87,88,89];
-let yellowBase = [144,145,146,147,148,149,159,160,161,162,163,164,174,175,176,177,178,179,189,190,191,192,193,194,204,205,206,207,208,209,219,220,221,222,223,224];
-let blueBase = [135,136,137,138,139,140,150,151,152,153,154,155,165,166,167,168,169,170,180,181,182,183,184,185,195,196,197,198,199,200,210,211,212,213,214,215];
+const redBase = [0,1,2,3,4,5,15,16,17,18,19,20,30,31,32,33,34,35,45,46,47,48,49,50,60,61,62,63,64,65,75,76,77,78,79,80];
+const greenBase = [9,10,11,12,13,14,24,25,26,27,28,29,39,40,41,42,43,44,54,55,56,57,58,59,69,70,71,72,73,74,84,85,86,87,88,89];
+const yellowBase = [144,145,146,147,148,149,159,160,161,162,163,164,174,175,176,177,178,179,189,190,191,192,193,194,204,205,206,207,208,209,219,220,221,222,223,224];
+const blueBase = [135,136,137,138,139,140,150,151,152,153,154,155,165,166,167,168,169,170,180,181,182,183,184,185,195,196,197,198,199,200,210,211,212,213,214,215];
 
 // ------ TOKEN BASE --------
 const basePockets = {
@@ -27,10 +20,10 @@ const basePockets = {
 };
 
 // --------- HOME PATHS ----------
-let redPath = [91,106,107,108,109,110,111];
-let greenPath = [22,23,37,52,67,82,97];
-let yellowPath = [113,114,115,116,117,118,133];
-let bluePath = [127,142,157,172,187,201,202];
+const redPath = [91,106,107,108,109,110,111];
+const greenPath = [22,23,37,52,67,82,97];
+const yellowPath = [113,114,115,116,117,118,133];
+const bluePath = [127,142,157,172,187,201,202];
 
 // ------- FULL GAME PATH ------
 const gamePath = [91, 92, 93, 94, 95, 96, 81, 66, 51, 36, 21, 6, 7, 8, 23, 38, 53, 68, 83, 98, 99, 100, 101, 102, 103, 104, 119, 134, 133, 132, 131, 130, 129, 128, 143, 158, 173, 188, 203, 218, 217, 216, 201, 186, 171, 156, 141, 126, 125, 124, 123, 122, 121, 120, 105, 90];
@@ -49,6 +42,29 @@ const tokenHomePath = {
     yellow: [118,117,116,115,114,113],
     blue: [202,187,172,157,142,127]
 };
+
+const startIndices = {red : 91, green : 23, yellow: 133, blue: 201};
+const safeSquares = new Set(Object.values(startIndices));
+
+// Dice Pattern
+const dicePatterns = {
+    1: [4],
+    2: [0, 8],
+    3: [0, 4, 8],
+    4: [0, 2, 6, 8],
+    5: [0, 2, 4, 6, 8],
+    6: [0, 2, 3, 5, 6, 8]
+};
+
+// ---- GAME STATE -----
+const gameState = {
+    currentPlayerIndex: 0,
+    players: ['red', 'green', 'yellow', 'blue'],
+    diceValue: null,
+    isGameOver: false,
+    canRoll: true,
+    isMoving: false
+}
 
 // -------- CREATE BOARD ----------
 function createBoard(){
@@ -107,6 +123,112 @@ function addTokens(){
     }
 }
 
+// ====== VALIDATION =======
+// RENDER DOTS ON THE DICE ----
+function renderDiceDots(number){
+    dice.innerHTML = '';
+
+    for(let i = 0; i < 9; i++){
+        const dotSlot = document.createElement('div');
+        
+        if(dicePatterns[number].includes(i)){
+            const dot = document.createElement('div');
+            dot.classList.add('dot');
+            dotSlot.appendChild(dot);
+        }
+        dice.appendChild(dotSlot);
+    }
+}
+
+// ----- CHECK IF PATH IS BLOCKED ------
+function isSquareBlocked(squareIndex, movingTokenColor){
+    const square = document.querySelector(`.square[data-index='${squareIndex}']`);
+    const tokensInSquare = square.querySelectorAll('.token');
+
+    if (tokensInSquare.length >= 2){
+        const firstTokenColor = tokensInSquare[0].dataset.color;
+
+        if (firstTokenColor !== movingTokenColor){
+            return true;
+        }
+    }
+    return false;
+}
+
+// ----- HOME PATH MOVEMENT ------
+function homePath(token, steps){
+    const color = token.dataset.color;
+    const currentHomeIndex = parseInt(token.dataset.pathIndex);
+    const maxIndex = tokenHomePath[color].length - 1;
+
+    if (currentHomeIndex + steps > maxIndex){
+        console.log("Roll is too high! You need an exact match to finish.");
+        return false;
+    }
+    return true;
+}
+
+// ----- CHECK IF ALL TOKENS HAVE REACHED END OF HOME PATH -----
+function checkVictory(color){
+    const finishedTokens = document.querySelectorAll(`.token.${color}-token.finished`);
+    if (finishedTokens.length === 4){
+        alert(`Hooray, ${color.toUpperCase()} HAS WON THE GAME!`);
+        gameState.isGameOver = true;
+        gameState.canRoll = false;
+    }
+}
+
+// ===== TURN & DICE CONTROL =====
+// ----- FUNCTION TO SWITCH PLAYER TURNS -----
+function switchTurn(){
+    gameState.currentPlayerIndex = (gameState.currentPlayerIndex + 1) % 4;
+    gameState.canRoll = true;
+    gameState.diceValue = null;
+
+    const nextPlayer = gameState.players[gameState.currentPlayerIndex];
+    dice.style.borderColor = nextPlayer;
+    console.log(`It is now ${nextPlayer}'s turn`);
+}
+
+// Function to display number on the dice
+function rollDice(){
+    if(!gameState.canRoll) return;
+
+    // animation
+    gameState.canRoll = false;
+    dice.classList.add('rolling');
+    dice.innerHTML = '';
+
+    setTimeout(() => {
+        dice.classList.remove('rolling');
+
+        let number = Math.floor((Math.random() * 6) + 1);
+        gameState.diceValue = number;
+        renderDiceDots(number);
+
+        setTimeout(() => {
+            const currentPlayer = gameState.players[gameState.currentPlayerIndex];
+            const tokensOnBoard = document.querySelectorAll(`.token.${currentPlayer}-token:not(.finished)`);
+            const hasTokensOnBoard = tokensOnBoard.length > 0;
+
+            if(number !== 6 && !hasTokensOnBoard){
+                console.log("No possible moves. Switching turn...");
+                gameState.diceValue = null;
+                switchTurn();
+            }else{
+                if(number === 6){
+                    console.log("Rolled a 6! Roll again.");
+                    gameState.canRoll = true;
+                }else{
+                    console.log("Select a piece to move.");
+                    gameState.canRoll = false;
+                }
+            }
+        }, 800);
+    }, 500);
+}
+
+// ====== MOVEMENT LOGIC ======
 // ---- Function to add click event to the tokens
 function handleTokenClick(event){
     const clickedToken = event.target;
@@ -129,8 +251,8 @@ function handleTokenClick(event){
     if(clickedToken.dataset.location === 'base'){
         if(gameState.diceValue === 6){
             moveFromBaseToStart(clickedToken);
-            gameState.diceValue = null;
-            gameState.canRoll = true;
+            //gameState.diceValue = null;
+            //gameState.canRoll = true;
         }
     }else{
         moveAlongPath(clickedToken, gameState.diceValue);
@@ -138,16 +260,19 @@ function handleTokenClick(event){
 }
 
 // Function to make the first move
-
-const startIndices = {red : 91, green : 23, yellow: 133, blue: 201};
-const safeSquares = new Set(Object.values(startIndices));
-
 function moveFromBaseToStart(clickedToken){
     const color = clickedToken.dataset.color;
     const startSquare = document.querySelector(`.square[data-index='${startIndices[color]}']`);
     startSquare.appendChild(clickedToken);
     clickedToken.dataset.location = 'path';
     clickedToken.dataset.pathIndex = 0;
+
+    if(gameState.diceValue === 6){
+        gameState.canRoll = true;
+    } else{
+        switchTurn();
+    }
+    gameState.diceValue = null;
 }
 
 // ---- MOVE TOKEN ALONG THE BOARD ----.
@@ -222,47 +347,7 @@ async function moveAlongPath(clickedToken, steps){
     gameState.diceValue = null;
 }
 
-// ----- CHECK IF TOKEN IS AT END OF HOME PATH ----
-function reachedEndOfHomePath(token){
-    console.log(`${token.dataset.color} piece ${token.dataset.pieceIndex} finished.`);
-
-    token.classList.add('finished');
-    token.removeEventListener('click', handleTokenClick);
-
-    token.innerHTML = '👑';
-    const pocketIndex = basePockets[token.dataset.color][token.dataset.pieceIndex];
-    const baseSquare = document.querySelector(`.square[data-index='${pocketIndex}']`);
-    
-    baseSquare.appendChild(token);
-
-    checkVictory(token.dataset.color);
-}
-
-// ----- CHECK IF ALL TOKENS HAVE REACHED END OF HOME PATH -----
-function checkVictory(color){
-    const finishedTokens = document.querySelectorAll(`.token.${color}-token.finished`);
-    if (finishedTokens.length === 4){
-        alert(`Hooray, ${color.toUpperCase()} HAS WON THE GAME!`);
-        gameState.isGameOver = true;
-        gameState.canRoll = false;
-    }
-}
-
-// ----- CHECK IF PATH IS BLOCKED ------
-function isSquareBlocked(squareIndex, movingTokenColor){
-    const square = document.querySelector(`.square[data-index='${squareIndex}']`);
-    const tokensInSquare = square.querySelectorAll('.token');
-
-    if (tokensInSquare.length >= 2){
-        const firstTokenColor = tokensInSquare[0].dataset.color;
-
-        if (firstTokenColor !== movingTokenColor){
-            return true;
-        }
-    }
-    return false;
-}
-
+// ===== RESOLUTION =====
 // ----- CAPTURE TOKEN -----
 function captureToken(token){
     const color = token.dataset.color;
@@ -293,80 +378,20 @@ function captureToken(token){
     return didCapture;
 }
 
-// ----- HOME PATH MOVEMENT ------
-function homePath(token, steps){
-    const color = token.dataset.color;
-    const currentHomeIndex = parseInt(token.dataset.pathIndex);
-    const maxIndex = tokenHomePath[color].length - 1;
+// ----- CHECK IF TOKEN IS AT END OF HOME PATH ----
+function reachedEndOfHomePath(token){
+    console.log(`${token.dataset.color} piece ${token.dataset.pieceIndex} finished.`);
 
-    if (currentHomeIndex + steps > maxIndex){
-        console.log("Roll is too high! You need an exact match to finish.");
-        return false;
-    }
-    return true;
-}
+    token.classList.add('finished');
+    token.removeEventListener('click', handleTokenClick);
 
-// Dice Pattern
-const dicePatterns = {
-    1: [4],
-    2: [0, 8],
-    3: [0, 4, 8],
-    4: [0, 2, 6, 8],
-    5: [0, 2, 4, 6, 8],
-    6: [0, 2, 3, 5, 6, 8]
-};
+    token.innerHTML = '👑';
+    const pocketIndex = basePockets[token.dataset.color][token.dataset.pieceIndex];
+    const baseSquare = document.querySelector(`.square[data-index='${pocketIndex}']`);
+    
+    baseSquare.appendChild(token);
 
-// Function to display number on the dice
-function rollDice(){
-    if(!gameState.canRoll) return;
-
-    // animation
-    gameState.canRoll = false;
-    dice.classList.add('rolling');
-    dice.innerHTML = '';
-
-    setTimeout(() => {
-        dice.classList.remove('rolling');
-
-        let number = Math.floor((Math.random() * 6) + 1);
-        gameState.diceValue = number;
-        renderDiceDots(number);
-
-        setTimeout(() => {
-            const currentPlayer = gameState.players[gameState.currentPlayerIndex];
-            const tokensOnBoard = document.querySelectorAll(`.token.${currentPlayer}-token[data-location="path"]`);
-            const hasTokensOnBoard = tokensOnBoard.length > 0;
-
-            if(number !== 6 && !hasTokensOnBoard){
-                console.log("No possible moves. Switching turn...");
-                gameState.diceValue = null;
-                switchTurn();
-            }else{
-                if(number === 6){
-                    console.log("Rolled a 6! Roll again.");
-                    gameState.canRoll = true;
-                }else{
-                    console.log("Select a piece to move.");
-                    gameState.canRoll = false;
-                }
-            }
-        }, 1000);
-    }, 500);
-}
-
-function renderDiceDots(number){
-    dice.innerHTML = '';
-
-    for(let i = 0; i < 9; i++){
-        const dotSlot = document.createElement('div');
-        
-        if(dicePatterns[number].includes(i)){
-            const dot = document.createElement('div');
-            dot.classList.add('dot');
-            dotSlot.appendChild(dot);
-        }
-        dice.appendChild(dotSlot);
-    }
+    checkVictory(token.dataset.color);
 }
 
 // ---- DICE ROLL EVENT LISTENER -----
@@ -375,34 +400,18 @@ dice.addEventListener('click', () => {
         rollDice();
     }
 });
-renderDiceDots(1);
 
-// ---- GAME STATE -----
-const gameState = {
-    currentPlayerIndex: 0,
-    players: ['red', 'green', 'yellow', 'blue'],
-    diceValue: null,
-    isGameOver: false,
-    canRoll: true,
-    positions: {
-        red: [16, 19, 61, 64],       
-        green: [25, 28, 70,73],
-        yellow: [160, 163, 205, 208],
-        blue: [151, 154, 196, 199]
-    }
-}
+// ------ BUTTON FOR PLAYING AGAINST HUMAN --------
+vsHuman.addEventListener('click', () => {
+  welcomeScreen.style.display = 'none';  
+  gameBoard.style.display = 'grid';
+  dice.style.display = 'grid';
+})
 
-// ----- FUNCTION TO SWITCH PLAYER TURNS -----
-function switchTurn(){
-    gameState.currentPlayerIndex = (gameState.currentPlayerIndex + 1) % 4;
-    gameState.canRoll = true;
-    gameState.diceValue = null;
-    const nextPlayer = gameState.players[gameState.currentPlayerIndex];
-    dice.style.borderColor = nextPlayer;
-    console.log(`It is now ${nextPlayer}'s turn`);
-}
+// Initialization
 createBoard();
 addTokens();
+renderDiceDots(1);
 
 // ---- MY CHEAT CODE TO MAKE TESTING EASIER -----
 const debug_Mode = true;
