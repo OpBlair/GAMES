209bac750cd69fbda2d1f0e6c3e6e2e301e7f97a
opblair <jusanotherthursday@gmail.com>
2026-05-09 18:48:11 +0300

    Drawn the board in a zig zag form.

--- Snake_&_Ladder/script.js
+++ Snake_&_Ladder/script.js
@@ -1,10 +1,24 @@
 'use strict';
 const gameBoard = document.getElementById('game-board');
 
-for(let a = 100; a > 0; a--){
+for(let row = 9; row >= 0; row--){
+    if(row % 2 === 0){
+        for(let col = 0; col < 10; col++){
+            let num = row * 10 + col + 1;
+            addSquare(num);
+        }
+    }else{
+        for(let col = 9; col >= 0; col--){
+            let num = row * 10 + col + 1;
+            addSquare(num);
+        }
+    }
+}
+
+function addSquare(number){
     let square = document.createElement('div');
     square.classList.add('square');
-    square.dataset.index = a;
-    square.textContent = a;
+    square.dataset.index = number;
+    square.textContent = number;
     gameBoard.appendChild(square);
 }
