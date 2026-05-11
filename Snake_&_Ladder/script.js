d94975433fcbcbe34957e3001abc718c2f393f9c
opblair <jusanotherthursday@gmail.com>
2026-05-11 17:52:18 +0300

    grouped some functions inside the Board class.

--- Snake_&_Ladder/script.js
+++ Snake_&_Ladder/script.js
@@ -1,7 +1,7 @@
 'use strict';
 const gameBoard = document.getElementById('game-board');
 
-class GAMERULES{
+class GameRules{
     constructor(){
         this.jumps = {
             ladders: [ [4, 14], [9, 31], [20, 38], [28, 84], [40, 59], [51, 67], [63, 81], [71, 91] ],
@@ -10,36 +10,46 @@ class GAMERULES{
     }
 }
 
-for(let row = 9; row >= 0; row--){
-    if(row % 2 === 0){
-        for(let col = 0; col < 10; col++){
-            let num = row * 10 + col + 1;
-            addSquare(num);
+class Board{
+    constructor(boardElement){
+        this.board = boardElement;
+    }
+
+    createBoard(){
+        for(let row = 9; row >= 0; row--){
+            if(row % 2 === 0){
+                for(let col = 0; col < 10; col++){
+                    let num = row * 10 + col + 1;
+                    this.createSquare(num);
+                }
+            }else{
+                for(let col = 9; col >= 0; col--){
+                    let num = row * 10 + col + 1;
+                    this.createSquare(num);
+                }
+            }
         }
-    }else{
-        for(let col = 9; col >= 0; col--){
-            let num = row * 10 + col + 1;
-            addSquare(num);
-        }
+    }
+
+    createSquare(number){
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.dataset.index = number;
+        square.textContent = number;
+        this.board.appendChild(square);
+    }
+
+    getSquareCenter(num){
+        const square = this.board.querySelector(`[data-index='${num}']`);
+        const boardRect = this.board.getBoundingClientRect();
+        const rect = square.getBoundingClientRect();
+
+        return{
+            x: rect.left - boardRect.left + rect.width / 2,
+            y: rect.top - boardRect.top + rect.height / 2
+        };
     }
 }
 
-function addSquare(number){
-    let square = document.createElement('div');
-    square.classList.add('square');
-    square.dataset.index = number;
-    square.textContent = number;
-    gameBoard.appendChild(square);
-}
-
-// GETTING SQUARE CENTER
-function getSquareCenter(num){
-    const square = document.querySelector(`[data-index='${num}']`);
-    const boardRect = gameBoard.getBoundingClientRect();
-    const rect = square.getBoundingClientRect();
-
-    return{
-        x: rect.left - boardRect.left + rect.width / 2,
-        y: rect.top - boardRect.top + rect.height / 2
-    };
-}
+const board = new Board(gameBoard);
+board.createBoard();
