52e7c194e1f5d5827885f94dcd3a90de3d63176c
opblair <jusanotherthursday@gmail.com>
2026-05-13 12:10:48 +0300

    Added snakes to the board.

--- Snake_&_Ladder/script.js
+++ Snake_&_Ladder/script.js
@@ -14,6 +14,7 @@ class Board{
     constructor(boardElement){
         this.board = boardElement;
         this.svg = document.getElementById('svg-overlay');
+        this.svg.setAttribute('viewBox', `0 0 ${this.board.clientWidth} ${this.board.clientHeight}`);
     }
 
     createBoard(){
@@ -100,6 +101,42 @@ class Board{
 
         this.svg.appendChild(line);
     }
+
+    // Snake
+    drawSnake(start, end){
+        const startPosition = this.getSquareCenter(start);
+        const endPosition = this.getSquareCenter(end);
+
+        const dx = endPosition.x - startPosition.x;
+        const dy = endPosition.y - startPosition.y;
+
+        // cp1 moves slightly right, cp2 moves slightly left
+        const cp1x = startPosition.x + dx * 0.2 + 40; 
+        const cp1y = startPosition.y + dy * 0.2;
+        const cp2x = startPosition.x + dx * 0.8 - 40;
+        const cp2y = startPosition.y + dy * 0.8;
+
+        const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+
+        // M = Move to start, Q = Quadratic Bézier curve (control point, then end point)
+        // const d = `M ${startPosition.x} ${startPosition.y} Q ${midX} ${midY} ${endPosition.x} ${endPosition.y}`;
+
+        // C = Cubic Bézier (cp1x cp1y, cp2x cp2y, endX endY)
+        const d = `M ${startPosition.x} ${startPosition.y} C ${cp1x} ${cp1y}, ${cp2x} ${cp2y}, ${endPosition.x} ${endPosition.y}`;
+
+        path.setAttribute('d', d);
+        path.setAttribute('stroke', 'green');
+        path.setAttribute('fill', 'transparent');
+        path.setAttribute('stroke-width', '6');
+        path.setAttribute('stroke-linecap', 'round');
+        path.setAttribute('stroke-dasharray', '1 4'); //scales.
+        path.setAttribute('filter', 'drop-shadow(2px 2px 2px rgba(0,0,0,0.3))');
+
+        this.svg.appendChild(path);
+
+        const angle = (Math.atan2(dx, dy) * 180 / Math.PI) - 90;
+        this.createSnakehead(startPosition.x, endPosition.y, angle);
+    }
 }
 
 const board = new Board(gameBoard);
@@ -110,5 +147,8 @@ const rules = new GameRules();
 rules.jumps.ladders.forEach(([start, end]) => {
     board.drawLadder(start, end);
 });
+rules.jumps.snakes.forEach(([start, end]) => {
+    board.drawSnake(start, end);
+});
 
-this.svg.setAttribute('viewBox', `0 0 ${this.board.clientWidth} ${this.board.clientHeight}`);
+//https://www.joshwcomeau.com/svg/friendly-introduction-to-svg/
